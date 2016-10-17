e2467d6891fa4bff910008ac3a0b28200b5b4b74
Quentin Donnellan <quentin@spideroak.com>
2016-10-16 21:47:59 -0500

    Began learning C the hard way.

---
+++ ex1-2/extra.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/* Some extra credit for ex1. */
+int main(int argc, char *argv[])
+{
+    printf("This is kinda cool... \n");
+    printf("Learning a new language \n");
+    printf("It is definitely different than python \n");
+    printf("But I can see the similarities. \n");
+    printf("Ooh, leaving of the new line\n");
+    printf("Really messes things up!");
+
+    // Note: When I leave off the \n from the last
+    // line it messes up the next terminal prompt,
+    // I wasn't expecting that...
+
+    return 0;
+}
