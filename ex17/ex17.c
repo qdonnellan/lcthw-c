0389ff5908e6620034577572ef77929976e9b6f8
Quentin Donnellan <quentin@spideroak.com>
2016-11-07 07:50:06 -0600

    Continued work on ex17

--- ex17/ex17.c
+++ ex17/ex17.c
@@ -15,7 +15,7 @@ struct Address {
 };
 
 struct Database {
-    struct Address row[MAX_ROWS];
+    struct Address rows[MAX_ROWS];
 };
 
 struct Connection {
@@ -37,6 +37,92 @@ void die(const char *message)
 void Address_print(struct Address *addr)
 {
     printf("%d, %s, %s\n", addr->id, addr->name, addr->email);
+}
+
+void Database_load(struct Connection *conn)
+{
+    int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
+    if (rc != 1) {
+        die("Failed to load database.");
+    }
+}
+
+struct Connection *Database_open(const char *filename, char mode) 
+{
+    struct Connection *conn = malloc(sizeof(struct Connection));
+    if (!conn) {
+        die("Memory error.");
+    }
+
+    conn->db = malloc(sizeof(struct Database));
+    if (!conn->db) {
+        die("Memory error.");
+    }
+
+    if (mode == 'c') {
+        conn->file = fopen(filename, "w");
+    } else {
+        conn->file = fopen(filename, "r+");
+
+        if (conn->file) {
+            Database_load(conn);
+        }
+    }
+
+    if (!conn->file) {
+        die("Faile to open file.");
+    }
+
+    return conn;
+}
+
+void Database_close(struct Connection *conn)
+{
+    if (conn) {
+        if (conn->file) {
+            fclose(conn->file);
+        }
+        if (conn->db) {
+            free(conn->db);
+        }
+
+        free(conn);
+    }
+}
+
+void Database_write(struct Connection *conn)
+{
+    int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
+    if (rc != 1) {
+        die("Failed to write database.");
+    }
+
+    rc = fflush(conn->file);
+    if (rc == -1) {
+        die("Cannot flush database.");
+    }
+
+}
+
+void Database_create(struct Connection *conn)
+{
+    int i = 0;
+
+    for (i = 0; i < MAX_ROWS; i++) {
+        // make a prototype to initialize it.
+        struct Address addr = {.id = 1, .set = 0};
+        conn->db->rows[i] = addr;
+    }
+}
+
+void Database_list(struct Connection *conn)
+{
+    int i = 0;
+    struct Database *db = conn->db;
+
+    for (i = 0; i < MAX_ROWS; i++) {
+        // I AM HERE.
+    }
 }
 
 
